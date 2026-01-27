ea8d467cac7f9c131aabc7c4fc41df0b4761e241
cubahai <haint23082003@gmail.com>
2026-01-27 22:44:51 +0700

    bai tap socket

---
+++ Socket/src/URL/URLConnectionExample.java
@@ -0,0 +1,29 @@
+package URL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLConnectionExample {
+
+		public static void main(String [] args) {
+			try {
+	         
+				URL url = new URL("https://www.qnu.com/");
+	            URLConnection urlConnection = url.openConnection();
+	            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	            StringBuilder sb = new StringBuilder();
+	            String line;
+	            while ((line = br.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	            System.out.println(sb.toString());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	
+	}
+
+}
